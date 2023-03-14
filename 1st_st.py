63e1429605ed1b676b9ad4e0ec655c71017440a0
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:11:23 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,18 +1,8 @@
 import streamlit as st
 import pandas as pd
 st.title ("INDIAN RAILWAYS")
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: url("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg")
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 st.caption("Safety|Security|Punctuality")
----st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
+st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
 st.balloons()
 
 
