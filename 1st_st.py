5dc87c52ac9876b1eaf7364dfe0fd554c0e9beab
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 22:46:06 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -19,7 +19,21 @@ st.balloons()
 import pandas as pd
 url = 'https://raw.githubusercontent.com/Vinotha27/Small_streamlit/main/train_schedule.csv'
 df = pd.read_csv(url, index_col=0)
+df.dropna(inplace=True)
 st.dataframe(df, use_container_width=True)
+
+# --- STREAMLIT SELECTION
+department = df['Station_name'].unique().tolist()
+ages = df['Departure_time'].unique().tolist()
+
+age_selection = st.slider('Station_name:',
+                        min_value= min(ages),
+                        max_value= max(ages),
+                        value=(min(ages),max(ages)))
+
+department_selection = st.multiselect('Departure_time:',
+                                    department,
+                                    default=department)
 
 
 
