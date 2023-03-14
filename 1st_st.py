4bea94c72095993f843095c91fcfcc384a889792
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 19:57:38 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
+st.image("https://th.bing.com/th?id=OIP.H3a0-BCqUlGBtQdYQ8JP6AHaFS&w=295&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2.jpg")
+
 
 
 
