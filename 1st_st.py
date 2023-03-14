650a6ff8331d55ec9bd3c26da9255ac4c72bbb80
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:08:02 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.title ("INDIAN RAILWAYS")
+(font color=‘red’,unsafe_allow_html=True)
 st.caption("Safety|Security|Punctuality")
 st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
 st.balloons()
