045064cd6faf4b79a74caadc80ace749e99a0da3
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:02:32 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+st.title ("INDIAN RAILWAYS")
 st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
 st.balloons()
 
