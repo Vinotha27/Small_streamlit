a189bf845e39589e562f301a8a52b3bc20740907
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:24:15 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -5,7 +5,8 @@ st.caption("Safety | Security | Punctuality")
 st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
 st.balloons()
 
-df=pd.read_csv("
+df=pd.read_csv("https://github.com/Vinotha27/Small_streamlit/blob/main/train_info.csv")
+df
 
 
 
