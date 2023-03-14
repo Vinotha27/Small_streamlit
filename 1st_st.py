f834ad4973c81b47a622d363c5b13423fcaee62d
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 19:00:08 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
-st.image("Indian Railway Image.jpg")
+st.image("https://images.assettype.com/swarajya/2020-03/629881c0-9cb4-485b-bb67-646a19eeeade/LGD_WAP7_30306.jpg?w=1280&q=100&fmt=pjpeg&auto=format.jpg")
 
 
