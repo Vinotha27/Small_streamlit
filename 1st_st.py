6b7dd7fd359593f4ec940dc09b903060b908b1fe
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 19:01:57 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
-st.image("https://images.assettype.com/swarajya/2020-03/629881c0-9cb4-485b-bb67-646a19eeeade/LGD_WAP7_30306.jpg?w=1280&q=100&fmt=pjpeg&auto=format.jpg")
+st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
+
 
 
