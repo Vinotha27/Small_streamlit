8a614ae2ce906a98eedfd62944ba715cea0e1ba1
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:54:22 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,18 +1,7 @@
 #import Packages
 import streamlit as st
 
-
-st.set_page_config(
-     page_title="Environment Data Atlas",
-     page_icon="🧊",
-     #layout="wide",
-     #initial_sidebar_state="expanded",
-     menu_items={
-         'Get Help': 'https://developers.snowflake.com',
-         'About': "This is an *extremely* cool app powered by Snowpark for Python, Streamlit, and Snowflake Data Marketplace"
-     }
-)
-
+#Heading
 st.header("INDIAN RAILWAYS")
 
 #Caption
@@ -26,10 +15,10 @@ st.balloons()
 
 
 #read file
-#import pandas as pd
-#url=('https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv')
-#df = pd.read_csv(url)
-#print(df.head(5))
+import pandas as pd
+url=('https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv')
+df = pd.read_csv(url)
+print(df.head(5))
 
 
 
