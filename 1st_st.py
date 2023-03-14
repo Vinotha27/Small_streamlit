fbbf57cb808e53091205fb1613eb4c5135c1b4ef
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:10:44 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -12,7 +12,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.caption("Safety|Security|Punctuality")
-st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
+---st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
 st.balloons()
 
 
