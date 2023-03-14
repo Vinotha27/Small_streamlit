98f56336bf17f56689d62b23bccf813540529b57
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:26:49 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,12 +1,23 @@
+#import Packages
 import streamlit as st
 import pandas as pd
+
+#Heading
 st.title ("INDIAN RAILWAYS")
+
+#Caption
 st.caption("Safety | Security | Punctuality")
+
+#Image
 st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
+
+#Baloon Display
 st.balloons()
 
-df=pd.read_csv("https://github.com/Vinotha27/Small_streamlit/blob/main/train_info.csv")
-df
+#read file
+url="https://github.com/Vinotha27/Small_streamlit/blob/main/train_info.csv"
+df = pd.read_csv(url,index_col=0)
+print(df.head(5))
 
 
 
