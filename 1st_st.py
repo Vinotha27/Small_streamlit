771f7c37e5704051739607cc75e3b27c50db9f64
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:39:43 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,8 +1,13 @@
 #import Packages
 import streamlit as st
 
-#Heading
-st.title ("INDIAN RAILWAYS")
+#title 
+st.set_page_config(
+     page_title="INDIAN RAILWAYS",
+     page_icon="https://th.bing.com/th?id=OIP.ca8nmh0N3R4U8fc7M5NytAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+     layout="wide",
+     initial_sidebar_state="expanded",
+)
 
 #Caption
 st.caption("Safety | Security | Punctuality")
@@ -12,6 +17,7 @@ st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd
 
 #Baloon Display
 st.balloons()
+
 
 #read file
 import pandas as pd
