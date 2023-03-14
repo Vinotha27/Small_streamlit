92805010be18d87c6e7c1ba617f204f097a09843
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 23:23:40 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -10,31 +10,14 @@ st.caption("Safety | Security | Punctuality")
 
 #Image
 #st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
-
-page_bg_img = f"""
+page_bg_img = '''
 <style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
-background-size: 180%;
-background-position: top left;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-[data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: fixed;
-}}
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
+body {
+background-image: url("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg");
+background-size: cover;
+}
 </style>
-"""
-
+'''
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 #Baloon Display
