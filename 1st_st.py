51f15d30b81d5713e28ce62401f529a8281e5e2d
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:19:20 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 st.title ("INDIAN RAILWAYS")
-original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">INDIAN RAILWAYS</p>'
 st.title(original_title, unsafe_allow_html=True)
 st.caption("Safety | Security | Punctuality")
 st.image("https://www.wallpapertip.com/wmimgs/155-1554570_indian-train-images-hd.jpg.jpg")
