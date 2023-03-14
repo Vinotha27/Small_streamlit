6606ffd4359f66220b12708e4027679d2cd77161
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:52:31 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -6,7 +6,7 @@ st.set_page_config(
      page_title="Environment Data Atlas",
      page_icon="🧊",
      layout="wide",
-     initial_sidebar_state="expanded",
+     #initial_sidebar_state="expanded",
      menu_items={
          'Get Help': 'https://developers.snowflake.com',
          'About': "This is an *extremely* cool app powered by Snowpark for Python, Streamlit, and Snowflake Data Marketplace"
