b6942d920cf22f36931e4a932b8eef4570358622
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-14 20:51:04 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -1,12 +1,15 @@
 #import Packages
 import streamlit as st
 
-st.header("INDIAN RAILWAYS")
-
-#title    
 st.set_page_config(
+     page_title="Environment Data Atlas",
+     page_icon="🧊",
      layout="wide",
      initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://developers.snowflake.com',
+         'About': "This is an *extremely* cool app powered by Snowpark for Python, Streamlit, and Snowflake Data Marketplace"
+     }
 )
 
 #Caption
