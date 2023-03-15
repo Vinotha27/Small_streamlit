62e613fb5a2c65a1d92cffcd83640f15212c73df
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-15 23:56:51 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -49,7 +49,6 @@ st.sidebar.header('User Input')
 
 #sidebar option
 st.sidebar.radio('Departure Time',['Before 18.00.00', '6.00.00  to 13.00.00 ', '13.00.00 to 18.00.00','After 18.00.00'])
-st.slider('Pick a Days', 'sunday','saturday')
 st.selectbox('Rounded Trip',['Yes','No'])
 
 
