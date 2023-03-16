0d9cfc2a456a8ca6e6539d614993746998cf8ab3
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-16 20:43:37 +0530

    Update 1st_st.py

--- 1st_st.py
+++ 1st_st.py
@@ -39,8 +39,8 @@ st.dataframe(merged, use_container_width=True)
 st.sidebar.header('User Input')
 
 #Demo page creation
-st.sidebar.radio('Departure Time',['Before 18.00.00', '6.00.00  to 13.00.00 ', '13.00.00 to 18.00.00','After 18.00.00'])
-if Departure Time is 'Before 18.00.00':
+d=st.sidebar.radio('Departure Time',['Before 18.00.00', '6.00.00  to 13.00.00 ', '13.00.00 to 18.00.00','After 18.00.00'])
+if d is 'Before 18.00.00':
     st.header('Departure_Time')
 
 st.sidebar.date_input('Travelling date')
